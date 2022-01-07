a74f87c8a71cec5a13f7f4c1c9c0af36a5851f95
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-01-07 21:48:57 +0100

    lite fix och animation

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 let githubDiv = document.getElementById("githubDiv");
 
-fetch("https://api.github.com/users/gamershabandari/repos")
-.then(response => response.json())
-.then(githubRepos => {
 
-    renderGithubDiv(githubRepos);
-});
+gsap.from(".min-bild", { y: -200, duration: 1, ease: "bounce"});
+gsap.from(".h1", { y: -1000, duration: 1.4, delay: 1, ease: "bounce"});
+
+gsap.from(".om-mig", { x: -1000, duration: 1.4, delay: 1.5, ease: "bounce"});
+gsap.from(".arbetsprover", { x: 1000, duration: 1.4, delay: 1.5, ease: "bounce"});
 
 
-function renderGithubDiv (repos){
 
-    let myRepos = repos;
-    console.log(myRepos);
-    
-    console.log(myRepos[0].id);
+// fetch("https://api.github.com/users/gamershabandari/repos")
+// .then(response => response.json())
+// .then(githubRepos => {
 
-    for (let i = 0; i < myRepos.length; i++) {
-        let repo = myRepos[i];
+//     renderGithubDiv(githubRepos);
+// });
 
-        console.log(repo.id);
-        console.log(repo.html_url);
 
-        let repoDiv = document.createElement("div");
+// function renderGithubDiv (repos){
 
-        let repoLink = document.createElement("a");
-        repoLink.innerText = repo.full_name;
-        repoLink.href = repo.html_url;
-        repoLink.target = "_blank";
-        repoLink.className = "repolink"
+//     let myRepos = repos;
 
-        repoDiv.append(repoLink);
-        githubDiv.append(repoDiv);
+//     for (let i = 0; i < myRepos.length; i++) {
+//         let repo = myRepos[i];
+
+//         console.log(repo.id);
+//         console.log(repo.html_url);
+
+//         let repoDiv = document.createElement("div");
+
+//         let repoLink = document.createElement("a");
+//         repoLink.innerText = repo.full_name;
+//         repoLink.href = repo.html_url;
+//         repoLink.target = "_blank";
+//         repoLink.className = "repolink"
+
+//         repoDiv.append(repoLink);
+//         githubDiv.append(repoDiv);
         
-    };
+//     };
 
-};
+// };
 
